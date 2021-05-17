9e18963642e360c397245be18ce81b59bfcb3b43
Vitalii Yarmus <vitalii.yarmys@binary-studio.com>
2021-05-17 12:43:37 +0300

    FE: added schema support for list, set, map

--- forward_engineering/helpers/propertyKeysHelper.js
+++ forward_engineering/helpers/propertyKeysHelper.js
@@ -22,14 +22,19 @@ const generatePropertyKeys = ({ traversalSource, collections, relationships, app
 };
 
 const getPropertyScript = (name, fieldData) => {
-    const dataType = getDataTypeClass(fieldData.childType || fieldData.type, fieldData.mode);
+    const dataType = getDataTypeClass({
+        type: fieldData.childType || fieldData.type,
+        mode: fieldData.mode,
+        subtype: fieldData.subtype,
+        firstItem: _.get(fieldData, 'properties.0'),
+    });
     const cardinality = _.toUpper(fieldData.propCardinality) || 'SINGLE';
 
     return `${name} = mgmt.makePropertyKey('${name}').dataType(${dataType}).cardinality(org.janusgraph.core.Cardinality.${cardinality}).make()`;
 };
 
-const getDataTypeClass = (dataType, mode) => {
-    switch (dataType) {
+const getDataTypeClass = ({ type, mode, subtype, firstItem }) => {
+    switch (type) {
         case 'string':
             return 'String.class';
         case 'boolean':
@@ -60,6 +65,53 @@ const getDataTypeClass = (dataType, mode) => {
         }
         case 'uuid':
             return 'UUID.class';
+        case 'map':
+            return 'HashMap.class';
+        case 'set':
+        case 'list':
+            return getTypeFromSubtype(subtype, firstItem);
+        default:
+            return 'Object.class';
+    }
+};
+
+const getTypeFromSubtype = (subType, firstItem) => {
+    const itemType = /<(.*?)>/.exec(subType)[1];
+
+    switch (itemType) {
+        case 'str':
+            return 'String[].class';
+        case 'char':
+            return 'Character[].class';
+        case 'bool':
+            return 'Boolean[].class';
+        case 'number':
+            switch (firstItem.mode) {
+                case 'byte':
+                    return 'Byte[].class';
+                case 'short':
+                    return 'Short[].class';
+                case 'int':
+                    return 'Integer[].class';
+                case 'long':
+                    return 'Long[].class';
+                case 'double':
+                    return 'Double[].class';
+                case 'float':
+                    return 'Float[].class';
+                default:
+                    return 'Integer[].class';
+            }
+        case 'date':
+            return 'Date[].class';
+        case 'uuid':
+            return 'UUID[].class';
+        case 'geoshape':
+            return 'Geoshape[].class';
+        case 'map':
+            return 'HashMap[].class';
+        case 'list':
+        case 'set':
         default:
             return 'Object.class';
     }
