cd470ff65f5cac9e79d6676172ae7c5054218b2d
Vitalii Yarmus <vitalii.yarmys@binary-studio.com>
2021-05-17 18:33:00 +0300

    FE: added apply to instance

--- forward_engineering/api.js
+++ forward_engineering/api.js
@@ -1,6 +1,7 @@
 const { setCommonDependencies } = require('./helpers/common');
 const { generateGremlinDataSamples } = require('./helpers/sampleDataHelper');
 const { generateJanusGraphSchema } = require('./helpers/schemaHelper');
+const gremlinHelper = require('./helpers/gremlinHelper');
 
 module.exports = {
     generateContainerScript(data, logger, cb, app) {
@@ -33,4 +34,44 @@ module.exports = {
             setTimeout(() => cb({ message: e.message, stack: e.stack }), 150);
         }
     },
+
+    applyToInstance(connectionInfo, logger, callback, app) {
+        logger.clear();
+        logger.log('info', connectionInfo, 'connectionInfo', connectionInfo.hiddenKeys);
+
+        const script = connectionInfo.script;
+
+        gremlinHelper
+            .connect(connectionInfo, app)
+            .then(() => gremlinHelper.applyToInstance(script))
+            .then(() => callback())
+            .catch(error => callback(prepareError(error)));
+    },
+
+    testConnection(connectionInfo, logger, callback, app) {
+        gremlinHelper
+            .connect(connectionInfo, app)
+            .then(() => {
+                gremlinHelper
+                    .testConnection()
+                    .then(() => {
+                        gremlinHelper.close();
+                        callback();
+                    })
+                    .catch(error => {
+                        gremlinHelper.close();
+                        logger.log('error', prepareError(error));
+
+                        callback({ message: 'Connection error', stack: error.stack });
+                    });
+            })
+            .catch(error => callback({ message: 'Connection error', stack: error.stack }));
+    },
+};
+
+const prepareError = error => {
+    return {
+        message: error.message,
+        stack: error.stack,
+    };
 };
